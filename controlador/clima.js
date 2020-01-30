853f8f905e51cb4d564421dc25d8a05b94b1c2ea
dgosoriog <46572605+dgosoriog@users.noreply.github.com>
2020-01-29 19:21:10 -0500

    Corregido y comentado

--- controlador/clima.js
+++ controlador/clima.js
@@ -1,8 +1,11 @@
-const axios = require('axios');
+const axios = require('axios');//importa la libreria axios
+//Funcion para obtener el clima de un lugar mediante una api.Recibe como parametros la latitud y longitud de una ciudad
+//Utiliza funcion asincrona
 const getClima = async(lat, lon) => {
     const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=ccea7499b77c6b76d2d67d22340e6dbb&units=metric`)
     return resp.data.main.temp;
 }
+//Exportar el modulo
 module.exports = {
     getClima
 }
