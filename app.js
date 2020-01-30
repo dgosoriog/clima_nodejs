a67bf8944c8b7bda28910d03db5f5d304b15dcbd
dgosoriog <46572605+dgosoriog@users.noreply.github.com>
2020-01-29 19:17:26 -0500

    Corregido y comentado

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const ubicacion = require('./controlador/ubicacion');
-const clima = require('./controlador/clima');
+const axios = require('axios');//importa libreria axios
+const ubicacion = require('./controlador/ubicacion');//importa modulo ubicacion
+const clima = require('./controlador/clima');//importa modulo clima
 //clima.getClima(-0.19, -78.5).then(console.log);
-//configurar como opciones
+//configurar parámetros
 const argv = require('yargs').options({
     nombre: {
         alias: 'n',
@@ -9,7 +10,8 @@ const argv = require('yargs').options({
         demand: true
     }
 }).argv;
-
+//Funcion para obtener el clima de una ciudad. Recibe como parametro el nombre de la ciudad
+//Utiliza funcion asincrona
 let getInfo = async(direccion) => {
     try {
         let ubc = await ubicacion.getLugarLatLong(direccion);
@@ -19,4 +21,5 @@ let getInfo = async(direccion) => {
         console.log(`No se pudo determinar el clima en ${direccion}`);
     }
 };
+//Prueba la funcion creada arriba.Imprime los resultados en consola
 getInfo(argv.nombre).then(res => { console.log(res); }).catch(err => console.log(err));
